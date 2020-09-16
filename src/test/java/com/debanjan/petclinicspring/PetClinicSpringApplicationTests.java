561c4ce67efa9c9b0e243dbedd593dca6d6887d1
Debanjan Mondal <mondal.debanjan.bwn@gmail.com>
2020-09-17 00:16:06 +0530

    Initial Project Add

---
+++ src/test/java/com/debanjan/petclinicspring/PetClinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debanjan.petclinicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
