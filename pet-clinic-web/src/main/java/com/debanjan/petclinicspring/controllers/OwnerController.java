05b44ff1c27d14a3755c8efb990fcfebe47ca6af
debanjan.mondal <mondal.debanjan.bwn@gmail.com>
2020-11-05 14:08:10 +0530

    Create Owner index page and controllers closes #13

---
+++ pet-clinic-web/src/main/java/com/debanjan/petclinicspring/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.debanjan.petclinicspring.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"owners","owners/index"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
